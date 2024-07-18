8fe80d687f636f42351ddff71abd0ac608fd10ab
Francisco Simas <145347669+FranciscoSimas@users.noreply.github.com>
2024-07-18 16:31:58 +0000

    Update queries.sql

--- SQL Files/queries.sql
+++ SQL Files/queries.sql
@@ -4,17 +4,20 @@ FROM Inscricoes I
 JOIN Estudantes E ON I.EstudanteID = E.ID
 JOIN Turmas T ON I.TurmaID = T.ID;
 
+
 -- Listar as turmas ministradas por um determinado professor
-SELECT T.Nome AS Turma, P.Nome AS Professor
-FROM Turmas T
-JOIN Professores P ON T.ProfessorID = P.ID
-WHERE P.Nome = 'Fábio Ferreira';
+SELECT Turmas.Nome
+FROM Turmas
+JOIN TurmasProfessores ON Turmas.ID = TurmasProfessores.TurmaID
+WHERE TurmasProfessores.ProfessorID = 1;
+
 
 -- Listar os estudantes com notas acima de um determinado valor
 SELECT E.Nome AS Estudante, I.NotaFinal
 FROM Inscricoes I
 JOIN Estudantes E ON I.EstudanteID = E.ID
 WHERE I.NotaFinal > 85;
+
 
 -- Listar as disciplinas de um determinado departamento
 SELECT D.Nome AS Disciplina, D.Departamento
@@ -27,11 +30,7 @@ WHERE D.Departamento = 'Matemática';
 
 
 
--- Listar as turmas ministradas por um determinado professor
-SELECT Turmas.Nome
-FROM Turmas
-JOIN TurmasProfessores ON Turmas.ID = TurmasProfessores.TurmaID
-WHERE TurmasProfessores.ProfessorID = 1;
+
 
 -- Listar as disciplinas de uma turma
 SELECT Disciplinas.Nome
@@ -39,3 +38,10 @@ FROM Disciplinas
 JOIN TurmasDisciplinas ON Disciplinas.ID = TurmasDisciplinas.DisciplinaID
 WHERE TurmasDisciplinas.TurmaID = 1;
 
+
+
+-- Listar as turmas ministradas por um determinado professor
+SELECT T.Nome AS Turma, P.Nome AS Professor
+FROM Turmas T
+JOIN Professores P ON T.ProfessorID = P.ID
+WHERE P.Nome = 'Carlos Santos';
